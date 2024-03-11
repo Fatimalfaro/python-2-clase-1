8a303049ab7ed23e5530a1519a6c045ca28cea3a
Fátima Alfaro <fatialfaro99@gmail.com>
2024-03-11 00:11:49 -0300

    Parte de la clase 3

---
+++ Clase 3/bucles.py
@@ -0,0 +1,27 @@
+'''
+edad=19
+
+while(edad <= 19):
+    print("Check")
+
+#El for es ideal para trabajar grupo de datos
+
+#Colecciones de datos / Tipos de datos complejos
+#[] Listas o arrays
+'''
+diasSemanas = [ #Posiciones    #Elementos
+    "Lunes",        #0              1
+    "Martes",       #1              2
+    "Miercoles",    #2              3
+    "Jueves",       #3              4
+    "Viernes",      #4              5
+    "Sabado",       #5              6
+    "Domingo"       #6              7
+    ]
+print(diasSemanas)
+
+#Selecciono un elemento por su indice
+
+diasSemanas[2]="Cualquier Cosa"
+print(diasSemanas)
+
